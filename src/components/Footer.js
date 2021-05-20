aee9dbf5cffc43916e53265a96f8b03c8164ab7b
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-05-20 13:26:53 +0300

    Change the registration page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,38 +7,41 @@ library.add(fab);
 
 export const Footer = () => {
   return (
-    <div className="layout-footer">
-      <div className="p-d-flex p-flex-row p-ai-center">
-        <a
-          href="https://creativecommons.org/licenses/by-sa/4.0/"
-          title="Creative Commons Attribution-ShareAlike 4.0 International License"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span className="hover-item">
-            <FontAwesomeIcon
-              className="p-mr-3"
-              icon={['fab', 'creative-commons']}
-              size="2x"
-            />
-            <FontAwesomeIcon
-              className="p-mr-3"
-              icon={['fab', 'creative-commons-by']}
-              size="2x"
-            />
-            <FontAwesomeIcon
-              className="p-mr-3"
-              icon={['fab', 'creative-commons-sa']}
-              size="2x"
-            />
-          </span>
-        </a>
-      </div>
-      <div className="p-d-flex p-flex-row p-ai-center">
-        <span className="p-mr-3">powered by</span>
-        <a href="https://scio.systems" rel="noreferrer" target="_blank">
-          <img src={LogoSCiO} alt="SCiO Logo" height="40px" />
-        </a>
+    <div className="footer">
+      <div className="footer-phantom"></div>
+      <div className="layout-footer">
+        <div className="p-d-flex p-flex-row p-ai-center">
+          <a
+            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            title="Creative Commons Attribution-ShareAlike 4.0 International License"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="hover-item">
+              <FontAwesomeIcon
+                className="p-mr-3"
+                icon={['fab', 'creative-commons']}
+                size="2x"
+              />
+              <FontAwesomeIcon
+                className="p-mr-3"
+                icon={['fab', 'creative-commons-by']}
+                size="2x"
+              />
+              <FontAwesomeIcon
+                className="p-mr-3"
+                icon={['fab', 'creative-commons-sa']}
+                size="2x"
+              />
+            </span>
+          </a>
+        </div>
+        <div className="p-d-flex p-flex-row p-ai-center">
+          <span className="p-mr-3">powered by</span>
+          <a href="https://scio.systems" rel="noreferrer" target="_blank">
+            <img src={LogoSCiO} alt="SCiO Logo" height="40px" />
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,17 +2,41 @@ import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
-import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import 'primereact/resources/primereact.min.css';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer } from '../components/Footer';
 import { Logo } from '../components/Logo';
 
 export const Register = () => {
-  const [acceptanceDialogOpen, setAcceptanceDialogOpen] = useState(false);
-  const [legalChecked, setLegalChecked] = useState(false);
-  const userImage = useRef(null);
+  // TODO: Default false.
+  // eslint-disable-next-line
+  const [formValid, setFormValid] = useState(true);
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  // eslint-disable-next-line
+  const [firstName, setFirstName] = useState('');
+  // eslint-disable-next-line
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailConfirm, setEmailConfirm] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  useEffect(() => {
+    // TODO: For register button to be enabled form also needs to be valid.
+    setFormValid(true);
+
+    // TODO: For error handling later on.
+    if (password !== passwordConfirm) {
+      setFormValid(false);
+      console.error('Invalid password');
+    }
+
+    if (email !== emailConfirm) {
+      setFormValid(false);
+      console.error('Invalid email');
+    }
+  }, [password, passwordConfirm, email, emailConfirm]);
 
   return (
     <>
@@ -20,137 +44,93 @@ export const Register = () => {
         <Logo />
         <div className="layout-content">
           <div className="p-grid">
-            <div className="p-col-12 p-md-8">
+            <div className="p-col-12 p-md-8" style={{ margin: '0 auto' }}>
               <div className="card p-fluid p-shadow-4 rounded">
-                <h5 className="p-text-center">New User Registration</h5>
-                <div className="p-formgrid p-grid">
+                <h5>New User Registration</h5>
+                <div className="p-formgrid p-grid p-mt-3">
                   <div className="p-field p-col-12 p-md-6">
                     <label htmlFor="firstname">First name</label>
-                    <InputText id="firstname" type="text" required />
+                    <InputText
+                      id="firstname"
+                      type="text"
+                      onChange={(e) => setFirstName(e.target.value)}
+                      required
+                    />
                   </div>
                   <div className="p-field p-col-12 p-md-6">
                     <label htmlFor="lastname">Last name</label>
-                    <InputText id="lastname" type="text" required />
+                    <InputText
+                      id="lastname"
+                      onChange={(e) => setLastName(e.target.value)}
+                      type="text"
+                      required
+                    />
                   </div>
                   <div className="p-field p-col-12 p-md-6">
                     <label htmlFor="email">Email</label>
-                    <InputText id="email" type="email" required />
+                    <InputText
+                      id="email"
+                      type="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
                   </div>
                   <div className="p-field p-col-12 p-md-6">
-                    <label htmlFor="confirmEmail">Repeat email</label>
-                    <InputText id="confirmEmail" type="email" required />
+                    <label htmlFor="emailConfirm">Repeat email</label>
+                    <InputText
+                      id="emailConfirm"
+                      onChange={(e) => setEmailConfirm(e.target.value)}
+                      type="email"
+                      required
+                    />
                   </div>
                   <div className="p-field p-col-12 p-md-6">
                     <label htmlFor="password">Password</label>
-                    <InputText id="password" type="password" />
-                  </div>
-                  <div className="p-field p-col-12 p-md-6">
-                    <label htmlFor="confirmPassword">Repeat password</label>
-                    <InputText id="confirmPassword" type="password" />
-                  </div>
-                </div>
-                <div className="p-formgrid p-grid p-justify-start p-mt-2">
-                  <div className="p-field p-col-12 p-md-6">
-                    <Button
-                      label="Acceptance of request pending"
-                      icon="pi pi-id-card"
-                      className="p-button-secondary p-mr-2 p-mb-2"
-                      onClick={() =>
-                        setAcceptanceDialogOpen(!acceptanceDialogOpen)
-                      }
-                    ></Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="p-col-12 p-md-4">
-              <div className="card p-fluid p-shadow-4 rounded p-text-center">
-                <h5>Profile picture</h5>
-                <div className="p-formgrid p-grid">
-                  <div className="p-field p-col-12 p-text-center">
-                    <img
-                      src="https://picsum.photos/350/350"
-                      height="215px"
-                      className="rounded"
-                      alt="Avatar"
+                    <InputText
+                      id="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
                     />
                   </div>
-                  <div className="p-field p-col-12">
-                    <Button
-                      label="Select image"
-                      icon="pi pi-image"
-                      className="p-button-secondary p-mr-2 p-mb-2"
-                      onClick={() => {
-                        userImage.current.click();
-                      }}
-                    ></Button>
-                  </div>
-                  <form>
-                    <input
-                      type="file"
-                      id="file"
-                      ref={userImage}
-                      style={{ display: 'none' }}
+                  <div className="p-field p-col-12 p-md-6">
+                    <label htmlFor="passwordConfirm">Repeat password</label>
+                    <InputText
+                      id="passwordConfirm"
+                      onChange={(e) => setPasswordConfirm(e.target.value)}
+                      type="password"
                     />
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div
-            className="p-col-12 p-text-left p-mt-2"
-            style={{ paddingLeft: '0' }}
-          >
+          <div className="p-col-12 p-text-center p-mt-1">
+            <div className="p-mb-5">
+              <Checkbox
+                inputId="legalCheck"
+                name="option"
+                value="Chicago"
+                checked={acceptedTerms}
+                onChange={() => {
+                  setAcceptedTerms(!acceptedTerms);
+                }}
+              />
+              <label
+                className="p-ml-2"
+                htmlFor="legalCheck"
+                style={{ cursor: 'pointer' }}
+              >
+                I accept the terms and conditions
+              </label>
+            </div>
             <Button
-              label="Register"
+              label="Create account"
               icon="pi pi-user-plus"
+              disabled={acceptedTerms === false}
               className="p-button-big p-mr-2 p-mb-2"
             ></Button>
           </div>
         </div>
-        {/* TODO: Make this reusable. */}
-        <Dialog
-          header="Please describe"
-          visible={acceptanceDialogOpen}
-          style={{ width: '400px' }}
-          modal
-          onHide={() => setAcceptanceDialogOpen(false)}
-        >
-          <div className="p-fluid">
-            <div className="p-formgrid p-grid">
-              <div className="p-field p-col-12">
-                <label htmlFor="organization">Your organization</label>
-                <InputText id="organization" type="text" required />
-              </div>
-              <div className="p-field p-col-12">
-                <label htmlFor="role">Your role</label>
-                <InputText id="role" type="text" required />
-              </div>
-              <div className="p-field p-col-12">
-                <div className="p-field-checkbox p-text-center">
-                  <Checkbox
-                    inputId="legalCheck"
-                    name="option"
-                    value="Chicago"
-                    checked={legalChecked}
-                    onChange={() => {
-                      setLegalChecked(!legalChecked);
-                    }}
-                  />
-                  <label htmlFor="legalCheck">Legal Notice</label>
-                </div>
-              </div>
-              <div className="p-field p-col-12 p-text-center">
-                <Button
-                  label="Make request"
-                  icon="pi pi-reply"
-                  className="p-mr-2 p-mb-2"
-                  onClick={() => setAcceptanceDialogOpen(false)}
-                ></Button>
-              </div>
-            </div>
-          </div>
-        </Dialog>
       </div>
       <Footer />
     </>
