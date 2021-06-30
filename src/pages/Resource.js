bc549567228150151b527901b8bd8effd713de20
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2021-06-30 16:49:55 +0300

    Adjust currentlyViewingTeam on Resource page

--- src/pages/Resource.js
+++ src/pages/Resource.js
@@ -1,15 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import EditResourceForm from '../components/forms/EditResourceForm';
 import Loading from '../components/Loading';
-import { getSingleResource } from '../services/teams';
-import { ToastContext } from '../store';
+import { getSingleResource, getSingleTeam } from '../services/teams';
+import { ToastContext, UserContext } from '../store';
 
 const Resource = () => {
   const { teamId, resourceId, mode } = useParams();
   const [resource, setResource] = useState(null);
   const [loading, setLoading] = useState(true);
   const { setError } = useContext(ToastContext);
+  const { setUser, currentlyViewingTeam } = useContext(UserContext);
 
   const loadResource = async (tId, rId) => {
     try {
@@ -22,6 +23,20 @@ const Resource = () => {
       setLoading(false);
     }
   };
+
+  const loadTeam = useCallback(async () => {
+    if (currentlyViewingTeam === null || currentlyViewingTeam.id !== teamId) {
+      setLoading(true);
+      const teamRes = await getSingleTeam(teamId);
+      setUser({ currentlyViewingTeam: teamRes.data });
+      setLoading(false);
+    }
+  }, [teamId, setUser]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // when mounted
+  useEffect(() => {
+    loadTeam();
+  }, [teamId]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // when mounted
   useEffect(() => {
