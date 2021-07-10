15dd3f0efcf12f415699cf879885bbf5208d6914
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2021-07-10 20:48:22 +0300

    Add AgroVoc autocomplete

--- src/components/forms/partials/ResourceClassification.js
+++ src/components/forms/partials/ResourceClassification.js
@@ -1,10 +1,13 @@
 import { Button } from 'primereact/button';
+import { AutoComplete } from 'primereact/autocomplete';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { Fieldset } from 'primereact/fieldset';
 import { InputText } from 'primereact/inputtext';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ToastContext } from '../../../store';
+import { searchAgroVoc } from '../../../services/integrations';
 
 const freeKeywordTemplate = (keyword) => ({
   scheme: 'free',
@@ -17,12 +20,37 @@ const freeKeywordTemplate = (keyword) => ({
 const ResourceClassification = ({ initialData, setter, mode }) => {
   const { t } = useTranslation();
   const [keywords, setKeywords] = useState(initialData.keywords ?? []);
+  const [results, setResults] = useState([]);
   const [kw, setKw] = useState('');
+  const { setError } = useContext(ToastContext);
 
   const addKeyword = () => {
-    const newKeywords = [...keywords, freeKeywordTemplate(kw)];
+    const newKw = typeof kw === 'object' ? kw : freeKeywordTemplate(kw);
+    const newKeywords = [...keywords, newKw];
     setKeywords(newKeywords);
     setKw('');
+  };
+
+  const handleError = (e) => {
+    let error = e && e.message;
+    const statusCode = e.response && e.response.status;
+    error =
+      statusCode === 422
+        ? e.response.data.errors[Object.keys(e.response.data.errors)[0]][0]
+        : e.response.data.error;
+    setError('Error', error);
+  };
+
+  const resultTemplate = (item) =>
+    `${item.taxon_name} [AgroVoc ID: ${item.taxon_id}]`;
+
+  const handleSearch = async (event) => {
+    try {
+      const { suggestions } = await searchAgroVoc(event.query);
+      setResults(suggestions);
+    } catch (error) {
+      handleError(error);
+    }
   };
 
   useEffect(() => {
@@ -33,12 +61,16 @@ const ResourceClassification = ({ initialData, setter, mode }) => {
     <div className="p-formgrid p-grid p-fluid">
       <div className="p-col-10">
         <div className="p-field">
-          <InputText
+          <AutoComplete
             disabled={mode === 'review'}
             name="keyword"
             value={kw}
-            type="text"
-            onChange={(e) => setKw(e.target.value)}
+            suggestions={results}
+            field="taxon_name"
+            completeMethod={handleSearch}
+            itemTemplate={resultTemplate}
+            selectedItemTemplate={resultTemplate}
+            onChange={(e) => setKw(e.value)}
           />
         </div>
       </div>
@@ -62,7 +94,16 @@ const ResourceClassification = ({ initialData, setter, mode }) => {
         className="p-mt-4"
         footer={keywordsFooter}
       >
-        <Column field="taxon_name" header={t('KEYWORD')} />
+        <Column
+          field="taxon_name"
+          header={t('KEYWORD')}
+          body={(rowData) => {
+            if (rowData.taxon_id !== '') {
+              return `${rowData.taxon_name} [AgroVoc ID: ${rowData.taxon_id}]`;
+            }
+            return rowData.taxon_name;
+          }}
+        />
       </DataTable>
     </Fieldset>
   );
