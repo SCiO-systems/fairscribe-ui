b2362c4071453ba49a2f40fb0078e4ad6754c0a4
Nasos Lamprokostopoulos <nlamprok@gmail.com>
2021-07-10 21:01:49 +0300

    Display UserPassword only if user is from scribe

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -1,23 +1,30 @@
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
-import React, { useContext, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useContext, useEffect, useState } from 'react';
 import RepoAccessManagerDialog from '../components/dialogs/RepoManagerAccessDialog';
 import Footer from '../components/Footer';
 import UserPassword from '../components/UserPassword';
 import UserProfile from '../components/UserProfile';
 import UserTargetedRepositories from '../components/UserTargetedRepositories';
+import { getUserProfile } from '../services/users';
 import { UserContext } from '../store';
 
-// TODO: Maybe initialise all stuff here and pass as props to components.
-
 const AccountSettings = () => {
-  // TODO: Default false.
-  // eslint-disable-next-line
-  const { t } = useTranslation();
   const [dialogOpen, setDialogOpen] = useState(false);
   const { id } = useContext(UserContext);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isScribeUser = async () => {
+    const { data } = await getUserProfile(id);
+    if (data.identity_provider === 'scribe') {
+      setShowPassword(true);
+    }
+  };
+
+  useEffect(() => {
+    isScribeUser();
+  }, []); // eslint-disable-line
 
   return (
     <>
@@ -31,7 +38,7 @@ const AccountSettings = () => {
           />
           {/* UI Preferences */}
           {/* <UserInterfacePreferences /> */}
-          <UserPassword userId={id} />
+          {showPassword && <UserPassword userId={id} />}
           {/* Targeted Repositories */}
           <UserTargetedRepositories userId={id} />
         </div>
