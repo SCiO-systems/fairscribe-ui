d33b080f1bc2bda94f5c5d09cac262ef9c85dcf6
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-07-12 12:39:19 +0300

    Fix issues with license wizard

--- src/components/dialogs/LicenseWizardDialog.js
+++ src/components/dialogs/LicenseWizardDialog.js
@@ -2,7 +2,7 @@ import { Accordion, AccordionTab } from 'primereact/accordion';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import { RadioButton } from 'primereact/radiobutton';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ccLicenses from '../../data/licenses/cc.json';
 import softwareLicenses from '../../data/licenses/software.json';
@@ -41,6 +41,8 @@ const findCCLicenseIndex = (attribution, commercial, derivative, sharing) => {
       // CC BY-NC 4.0
       return 4;
     }
+    // CC BY-NC 4.0
+    return 4;
   }
 
   // CC BY 4.0
@@ -56,6 +58,21 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
   const [shareAdaptations, setShareAdaptations] = useState(null);
   const [step, setStep] = useState(0);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [nextButtonHidden, setNextButtonHidden] = useState(false);
+
+  useEffect(() => {
+    setNextButtonHidden(false);
+    const flowEndIndexes = [0, 6, 3];
+    const licenseIndex = findCCLicenseIndex(
+      attribution,
+      commercialUse,
+      derivativeWorks,
+      shareAdaptations
+    );
+    if (flowEndIndexes.indexOf(licenseIndex) > -1 || step === 4) {
+      setNextButtonHidden(true);
+    }
+  }, [attribution, commercialUse, derivativeWorks, shareAdaptations, step]);
 
   const resetState = () => {
     setSoftware(null);
@@ -72,7 +89,7 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
     setDialogOpen(false);
   };
 
-  const NavigationButtons = () => (
+  const NavigationButtons = ({ nextHidden }) => (
     <div className="p-mt-4 p-d-flex p-jc-between">
       <Button
         label="Previous"
@@ -82,14 +99,16 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
         }}
         style={{ width: '125px' }}
       />
-      <Button
-        label="Next"
-        onClick={() => {
-          setStep(step + 1);
-          setActiveIndex(activeIndex + 1);
-        }}
-        style={{ width: '125px' }}
-      />
+      {!nextHidden && (
+        <Button
+          label="Next"
+          onClick={() => {
+            setStep(step + 1);
+            setActiveIndex(activeIndex + 1);
+          }}
+          style={{ width: '125px' }}
+        />
+      )}
     </div>
   );
 
@@ -179,7 +198,7 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
                   choice2Text="No. Anyone can use my work, even without giving me
                   attribution."
                 />
-                <NavigationButtons />
+                <NavigationButtons nextHidden={nextButtonHidden} />
               </AccordionTab>
               <AccordionTab
                 disabled={activeIndex !== 2}
@@ -193,7 +212,7 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
                   choice1Text="Yes. Others can use my work, even for commercial purposes."
                   choice2Text="No. Others can not use my work for commercial purposes."
                 />
-                <NavigationButtons />
+                <NavigationButtons nextHidden={nextButtonHidden} />
               </AccordionTab>
               <AccordionTab
                 disabled={activeIndex !== 3}
@@ -207,7 +226,7 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
                   choice1Text="Yes. Others can remix, adapt, or build upon my work."
                   choice2Text="No. Others may only use my work in unadapted form."
                 />
-                <NavigationButtons />
+                <NavigationButtons nextHidden={nextButtonHidden} />
               </AccordionTab>
               <AccordionTab
                 disabled={activeIndex !== 4}
@@ -221,7 +240,7 @@ const LicenseWizardDialog = ({ dialogOpen, setDialogOpen, setLicense }) => {
                   choice1Text="Yes. Others can share adaptations of my work under any terms."
                   choice2Text="No. Others must use the same CC license if they adapt my work."
                 />
-                <NavigationButtons />
+                <NavigationButtons nextHidden={nextButtonHidden} />
               </AccordionTab>
             </Accordion>
           )}
