ad7a49db7d3e384e0c89786854b6732a7128e875
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-09-16 16:19:57 +0300

    Unset debug mode for i18next

--- src/utilities/i18n-next.js
+++ src/utilities/i18n-next.js
@@ -3,10 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import en from '../translations/en/index';
 
 i18n.use(initReactI18next).init({
-  resources: {
-    en,
-  },
-  debug: true,
+  resources: { en },
   lng: 'en',
   keySeparator: false,
   fallbackLng: 'en',
