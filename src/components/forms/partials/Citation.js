e7f2e3afc739db500df238dc7f7f2b08c23a1801
Konstantinos Lypitkas <klipitkas@gmail.com>
2021-10-11 19:49:15 +0300

    Refactor citation to use re-usable components

--- src/components/forms/partials/Citation.js
+++ src/components/forms/partials/Citation.js
@@ -1,27 +1,18 @@
-import { InputTextarea } from 'primereact/inputtextarea';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import SimpleTextArea from '../../fields/SimpleTextArea';
 
 const Citation = ({ mode, citation, setCitation }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="p-fluid">
-      <div className="p-formgrid p-grid">
-        <div className="p-field p-col-12 p-md-12">
-          <label htmlFor="name">{t('CITATION')}</label>
-          <InputTextarea
-            disabled={mode === 'review'}
-            id="name"
-            type="text"
-            value={citation}
-            rows={5}
-            onChange={(e) => setCitation(e.target.value)}
-            required
-          />
-        </div>
-      </div>
-    </div>
+    <SimpleTextArea
+      mode={mode}
+      title={t('CITATION')}
+      text={citation}
+      setText={setCitation}
+      className="p-mb-4"
+    />
   );
 };
 
